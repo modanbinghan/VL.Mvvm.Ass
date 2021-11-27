5016b87c5074d9af041459938654bad0c14ed9f1
modanbinghan <zhangyining128@163.com>
2021-11-27 11:41:04 +0800

    IAsyncContext/AsyncContext,ObservableObject,RelayCommand are added.

---
+++ src/VL.Mvvm.Ass/VL.Mvvm.Ass/IAsyncContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace VL.Mvvm.Ass
+{
+    public interface IAsyncContext
+    {
+        /// 
+        /// Get the context of the creator thread
+        /// 
+        SynchronizationContext AsynchronizationContext { get; }
+
+        /// 
+        /// Test if the current executing thread is the creator thread
+        /// 
+        bool IsAsyncCreatorThread { get; }
+
+        /// 
+        /// Post a call to the specified method on the creator thread
+        /// 
+        /// Method that is to be called
+        /// Method parameter/state
+        void AsyncPost(SendOrPostCallback callback, object state);
+    }
+}
---
+++ src/VL.Mvvm.Ass/VL.Mvvm.Ass/ObservableObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace VL.Mvvm.Ass
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged 接口
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
